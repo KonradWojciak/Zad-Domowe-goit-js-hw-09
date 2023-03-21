ecfa243a9fdeecf3d00db3d2841773ba8f0d6cc9
KonradWojciak <konradwojciak@gmail.com>
2023-03-21 22:58:56 +0100

    for check

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,21 +1,9 @@
-async function fetchCountries() {
-  const inputValue = inputBox.value.trim();
-  const url = `https://restcountries.com/v3.1/all`;
-  const searchParams = {
-    param1: '_name.official',
-    param2: '_capital',
-    param3: '_population',
-    param4: '_flags.svg',
-    param5: '_languages',
-  };
+async function fetchCountries(value) {
+  const response = await fetch(
+    `https://restcountries.com/v3.1/name/${value}?fields=name,capital,population,flags,languages`
+  );
 
-  const queryParams = new URLSearchParams(searchParams);
-  const stringParams = queryParams.toString();
-  const urlWithParams = `${url}?${stringParams}`;
-
-  const countries = await fetch(urlWithParams);
-
-  return countries.catch(error => console.log(error));
+  return response.json();
 }
 
 export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
+import debounce from 'lodash.debounce';
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { fetchCountries } from 'fetchCountries.js';
+import { fetchCountries } from './fetchCountries';
+
+const countryInfo = document.querySelector('.country-info');
+const inputBox = document.querySelector('input');
+const DEBOUNCE_DELAY = 300;
 
 const countryList = document.querySelector('.country-list');
 
-const countryInfo = document.querySelector('.country-info');
-
-const DEBOUNCE_DELAY = 300;
-
-const inputBox = document.querySelector('input');
-
-async function makeCountryList() {
-  const li = document.createElement('li');
-  li.innerHTML = `
+async function makeCountryList(countries) {
+  countries.forEach(country => {
+    const li = document.createElement('li');
+    li.innerHTML = `
 <img src="${country.flags.svg}" alt="${country.name.official} flag">
 <span style="font-weight: bold;">${country.name.official}</span>
 `;
-  countryList.appendChild(li);
+    countryList.appendChild(li);
+  });
 }
 
-async function makeCountryInfo() {
+async function makeCountryInfo(country) {
   const info = document.createElement('div');
   info.innerHTML = `
 <h2>${country.name.official}</h2>
@@ -48,10 +49,8 @@ inputBox.addEventListener(
         Notify.info('To many matches found. Please enter more specific name');
       } else if (countries.length > 1) {
         makeCountryList(countries);
-      } else if (countryName === inputValue) {
-        makeCountryInfo(countries);
-      } else if (inputValue.includes(countryName)) {
-        makeCountryList(countries);
+      } else if (countryName.length === 1) {
+        makeCountryInfo(countries[0]);
       } else {
         Notify.failure('Oops, there is no country with that name');
       }
